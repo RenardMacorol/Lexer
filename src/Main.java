05a55bee9371351da352d57ca79105d952351f64
RenardMacorol <rmacorol18@gmail.com>
2024-11-04 12:08:31 +0800

    add file support

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
+import java.nio.file.Paths;
 
 public class Main {
 	public static void main(String[] args) {
-		String[] input = { "12 + 34 * (56 - 78 ) /90", "(sum + 47) / total" };
-
-		Lexer lexer = new Lexer(input[0]);
-		List<Token> tokens = lexer.tokenize();
-		Lexer lexer1 = new Lexer(input[1]);
-		List<Token> tokens1 = lexer1.tokenize();
-
-		for (Token token : tokens) {
-			System.out.println(token);
-		}
-		System.out.println("-------------------------");
-		for (Token token : tokens1) {
-			System.out.println(token);
+		String filePath = "../input.txt";
+		try {
+			List<String> lines = Files.readAllLines(Paths.get(filePath));
+			String input = String.join(" ", lines);
+			System.out.println("Initializing the Lexer");
+			Lexer lexer = new Lexer(input);
+			List<Token> tokens = lexer.tokenize();
+			for (Token token : tokens) {
+				System.out.println(token);
+			}
+		} catch (IOException e) {
+			System.out.println("Error Reading Files: " + e.getMessage());
+		} catch (RuntimeException e) {
+			System.out.println("Error: " + e.getMessage());
 		}
 
 	}
